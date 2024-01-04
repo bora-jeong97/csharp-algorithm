10f9fdd53f2b1008fbf59c61565923842eb300f7
bora-jeong97 <purple7714@naver.com>
2024-01-05 00:38:11 +0900

    SideWinder 미로 생성 알고리즘

--- Board.cs
+++ Board.cs
@@ -29,11 +29,12 @@ namespace Algorithm
             _tile = new TileType[size, size];
             _size = size;
 
-
-            GenerateByBinaryTree();
+            // Mazes for Programmers 책
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
 
         }
-    
+
         void GenerateByBinaryTree()
         {
             // Mazes for Programmers
@@ -87,6 +88,72 @@ namespace Algorithm
                 }
             }
         }
+
+        void GenerateBySideWinder()
+        {
+            // 먼저 길을 다 막아버린다.
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+
+                }
+            }
+
+            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            // Binary Tree Algorithm
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                int count = 1;  // 연속된 점의 개수
+
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    if (y == _size - 2) // 아래 외각 바로 전에 위치할 때는
+                    {
+                        _tile[y, x + 1] = TileType.Empty;   // 무조건 우측으로 가도록 함.
+                        continue;
+                    }
+
+                    if (x == _size - 2) // 오른쪽 외각 바로 전에 위치할 때는
+                    {
+                        _tile[y + 1, x] = TileType.Empty;   // 무조건 아래측으로 가도록 함.
+                        continue;
+                    }
+
+
+
+                    // 연속해서 뚫린 길 중 하나를 선택해서 길을 뚫는다.
+
+
+                    if (rand.Next(0, 2) == 0)   // 0~1
+                    {
+                        _tile[y, x + 1] = TileType.Empty;   // 우측
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;   // 아래측 *2를 해주는 이유는 2칸에 1칸이 벽이기 때문
+                        count = 1; // 다시 초기화
+
+                    }
+
+                }
+            }
+        }
+
 
         public void Render()
         {
