de2e33a07c67a3ceac2ba493039349f07302adca
bora-jeong97 <purple7714@naver.com>
2024-01-09 05:56:24 +0900

    BFS를 이용한 미로 최단거리 길찾기 구현

--- Player.cs
+++ Player.cs
@@ -40,6 +40,71 @@ namespace Algorithm
             PosX = posX;
             _board = board; // 내부에서 저장
 
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0 };   // Dir방향을 기준으로 위왼아오
+            int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+            bool[,] found = new bool[_board.Size, _board.Size]; // 좌표
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>(); // 선입 선출
+            q.Enqueue(new Pos(PosY, PosX));  // 초기 시작점을 넣어준다.
+            found[PosY, PosX] = true;        // 방문 체크
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (q.Count > 0)
+            {
+                // 맨 앞 제거 
+                Pos pos = q.Dequeue();  
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++) // 4 방향
+                {
+                    // next 좌표 뽑기 
+                    int nextY = nowY + deltaY[i];   
+                    int nextX = nowX + deltaX[i];
+
+
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+                    // a.인접하지 않았으면 스킵
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    // b.이미 발견한 애라면 스킵
+                    if (found[nextY, nextX])
+                        continue;
+
+                    // 맨 뒤 추가
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+
+                }
+            }
+
+            // 목적지 체크
+            int y = _board.DestY;
+            int x = _board.DestX;
+            while(parent[y, x].Y != y || parent[y, x].X != x)   // 자식과 부모의 좌표가 같을 때까지 == 시작점까지 되짚어가기
+            {
+                _points.Add(new Pos(y, x)); // 내 좌표
+                Pos pos = parent[y, x]; // 부모 좌표
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x)); // 첫 좌표 넣어줌.
+            _points.Reverse(); // 역으로 바꿔줌.
+
+        }
+
+        // 우수법
+        void RightHand()
+        {
             // 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
             int[] frontY = new int[] { -1, 0, 1, 0 };
             int[] frontX = new int[] { 0, -1, 0, 1 };
@@ -50,7 +115,7 @@ namespace Algorithm
 
 
             // 목적지 도착하기 전에는 계속 실행
-            while (PosY != board.DestY || PosX != board.DestX)
+            while (PosY != _board.DestY || PosX != _board.DestX)
             {
                 // 우수법
                 // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는 지 확인.
@@ -66,7 +131,7 @@ namespace Algorithm
 
                 }
                 // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
-                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty) 
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
                 {
                     // 앞으로 한 보 전진
                     PosX = PosX + frontX[_dir];
@@ -84,7 +149,7 @@ namespace Algorithm
         }
 
 
-        const int MOVE_TICK = 10; // 0.1초 100ms
+        const int MOVE_TICK = 100; // 0.1초 100ms
         int _sumTick = 0;
         int _lastIndex = 0;
         public void Update(int deltaTick)   // deltaTick : 이전 시간과 현재 시간의 차이
